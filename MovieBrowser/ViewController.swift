5526be197e7ea39ea92956a7f25c47e0ece70482
Jeet Gandhi <jeet.gandhi@northgateps.com>
2018-02-12 21:42:40 +0530

    Initial Commit

---
+++ MovieBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieBrowser
+//
+//  Created by Rave on 12/02/18.
+//  Copyright © 2018 Jeet Gandhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,27 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'MovieBrowser' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+pod 'IQKeyboardManagerSwift'
+
+pod 'SCLAlertView'
+
+pod 'ObjectMapper'
+
+pod 'Alamofire', '~> 4.5'
+
+pod 'AlamofireImage', '~> 3.3'
+
+pod 'SDWebImage', '~> 4.0'
+
+pod 'Result'
+
+pod 'AlamofireObjectMapper'
+
+pod 'Moya'
+  # Pods for MovieBrowser
+
+end
